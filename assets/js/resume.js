de671a3ca8945bd2129875a66de58ebb4f9de3e6
zeyap <zeyap@qq.com>
2018-03-21 19:18:23 +0800

    resumeee

--- assets/js/resume.js
+++ assets/js/resume.js
@@ -1,6 +1,4 @@
 ﻿var skills = {
-    "technical": ["C/C++", "Unity3D with C#", "HTML/CSS", "JavaScript", "SQL", "Maya", "Photoshop"],
-    "research": ["MATLAB & Psychtoolbox", "SPSS"],
     "language": ["English (TOEFL iBT 108)", "Chinese Mandarin (Native)"],
 };
 
@@ -44,6 +42,10 @@ var json = {
         },
     ],
     "experiences": [[
+        
+
+    ],[
+        
         {
             "title": "Global Game Jam - Game Dev Hackathon",
             "role": "Lead Developer",
@@ -68,20 +70,6 @@ var json = {
                 "Designed user surveys to conduct a testing session on usability and effectiveness of the game series",
             ],
         },
-        {
-            "title": "Re-designing Gesture Set for Full-body User Interface",
-            "role": "Research Assistant",
-            "institute": "Natural Interaction Lab @ ZJU",
-            "city": "Hangzhou",
-            "country": "China",
-            "duration": "Jul. 2015 - Dec. 2015",
-            "bulletpoints": [
-                "Designed and conducted an experiment where participants intuitively generated gestures (i.e. by user-defined method) and evaluated those gestures by popularity, subjective ratings and physiological risk",
-                "Proposed a gesture set for whole-body human-computer interface similar to Kinect, based on the research findings",
-            ]
-        },
-
-    ],[
         {
             "title": "Inverse World - AR Game",
             "role": "Art Designer",
@@ -109,7 +97,7 @@ var json = {
 };
 
 var json_cn = {
-    "titles":["教育背景","技能/证书","工作&学术经历","相关项目","擅长技术","相关课程","语言能力","论文发表"],
+    "titles":["教育背景","技能/证书","工作&学术经历","项目经历","擅长技术","相关课程","语言能力","论文发表"],
     "contact": {
         "email": "zeyapeng@outlook.com",
         "address": "杭州市西湖区浙江大学西溪校区北园",
@@ -123,61 +111,52 @@ var json_cn = {
             "gpa": "3.65/4.00",
             "duration": "2013年9月 - 2018年6月",
         },
-        {
-            "name": "加州大学戴维斯分校",
-            "degree": "暑期课程项目",
-            "city": "Davis",
-            "country": "CA, USA",
-            "gpa": "3.65/4.00",
-            "duration": "2014年8月 - 2014年9月",
-        },
     ],
     
     "experiences": [[
+
+    ], [
+        {
+            "title": "听歌识曲应用",
+            "role": "独立开发",
+            "city": "杭州",
+            "country": "",
+            "duration": "2017年11月 - 2018年1月",
+            "bulletpoints": [
+                "作为数字视音频处理课程项目，在Qt Creator上用C++独立开发了听歌识曲应用的原型，借助FFTW3和QMultimedia库实现了音频分析，独立实现了乐纹提取、数据库存储、片段搜索和排序的功能，达到了较好的识曲效果",
+            ]
+        },
         {
             "title": "Global Game Jam",
             "role": "游戏主程",
             "institute": "椰岛游戏",
             "city": "上海",
             "country": "",
-            "duration": "2018年2月 - 2018年2月",
+            "duration": "2018年2月",
             "bulletpoints": [
-                "和组员共同设计并用Unity3D制作了约会模拟游戏 <a href='https://globalgamejam.org/2018/games/dating-starts'>Dating Starts</a>，游戏中实现了丰富的交互操作与视觉反馈。最终作品被承办方（CiGa）公众号收录",
+                "和组员共同设计并用Unity3D制作了约会模拟游戏 <a href='https://globalgamejam.org/2018/games/dating-starts'>Dating Starts</a>，负责设计和实现交互操作与视觉反馈。最终作品被承办方（CiGa）公众号收录",
             ]
         },
         {
-            "title": "伊利诺伊香槟分校暑期科研",
+            "title": "空间想象能力训练游戏开发",
             "role": "游戏开发",
             "institute": "Cascade Lab, CS @ UIUC",
             "city": "Urbana",
             "country": "IL, USA",
             "duration": "2017年7月 - 2017年9月",
             "bulletpoints": [
-                "设计并用Unity3D开发了训练大学新生空间想象力的3D游戏",
+                "设计并用Unity3D开发了训练大学新生空间想象力的3D游戏，游戏平台具有较好的可拓展性，并且每个游戏可以记录玩家行为",
                 "在10名学生中进行测试，游戏模块记录的结果支持了被试在标准化测试中显示的空间想象能力和游戏表现有着0.9左右的相关，该成果发表于ACM IUI'18会议",
             ],
         },
-        
-
-    ], [
         {
-            "title": "听歌识曲应用（数字视音频处理课程项目）",
-            "role": "独立开发",
-            "city": "杭州",
-            "country": "",
-            "duration": "2017年11月 - 2018年1月",
-            "bulletpoints": [
-                "在Qt Creator上用C++独立开发了听歌识曲应用的原型，借助FFTW3和QMultimedia库实现了音频分析，独立实现了乐纹提取、数据库存储、片段搜索和排序的功能，达到了较好的识曲效果",
-            ]
-        },
-        {
-            "title": "游戏程序设计课程项目",
+            "title": "逆向世界-AR游戏",
             "role": "美术和交互设计",
             "city": "杭州",
             "country": "",
             "duration": "2017年4月 - 2017年7月",
             "bulletpoints": [
-                "在Unity3D上共同设计并开发了AR游戏《逆向世界》，负责完成了人物造型设定，制作了游戏中所有的2D素材，完成了和交互相关的代码，学习并使用Unity Shaderlab进行游戏过场动画的特效处理",
+                "作为游戏程序设计课程项目，在Unity3D上共同设计并开发了AR游戏《逆向世界》，负责完成了人物造型设定，制作了游戏中所有的2D素材，完成了和交互相关的代码，学习并使用Unity Shaderlab进行游戏过场动画的特效处理",
             ]
         },
         {
@@ -187,8 +166,8 @@ var json_cn = {
             "country": "",
             "duration": "2016年11月 - 2017年6月",
             "bulletpoints": [
-                "设计并完成了分类学习的认知心理学研究，通过30名被试的眼动实验发现个体学习策略与其工作记忆水平相关",
                 "在可参考资料很少的情况下，使用上海青研提供的眼动仪SDK通过MATLAB编程进行了眼动实验，目前该程序还在由实验室成员维护和使用",
+                "设计并完成了分类学习的认知心理学研究，通过30名被试的眼动实验发现个体学习策略与其工作记忆水平相关",
             ]
         },
     ]],
@@ -253,6 +232,7 @@ function formatResume(jsonObj) {
     document.getElementById("exp" + sectitlePos.toString()).innerHTML = titles[3];
     var elems = {"html":["","","",""],};
 
+    var endOfExp1;
     for (var i = 0; i < exps.length; i++) {
         
         for (var j = 0; j < exps[i].length; j++) {
@@ -276,11 +256,14 @@ function formatResume(jsonObj) {
             }
             elems["html"][colNo - 1] += item;
         }
+        if (i == 0) { endOfExp1 = colNo; }
+            
     }
-    elems["html"][0] += '<h4 class="timeline-title">' + titles[7] + '</h4>';
+
+    elems["html"][endOfExp1] += '<h4 class="timeline-title">' + titles[7] + '</h4>';
     for (var i = 0; i < publications.length; i++) {
         var pubitem = "<p>" + publications[i].title + " " + publications[i].journal + " " + publications[i].issue + "</p>";
-        elems["html"][0] += pubitem;
+        elems["html"][endOfExp1] += pubitem;
     }
     for (var i = 1; i <= 3; i++) {
         //console.log(elems["html"][i]);
@@ -320,10 +303,10 @@ function drawDonut_other() {
                     }
                 },
                 data: [
-                    { value: 20, name: 'Interaction Design' },
-                    { value: 20, name: 'Data Analysis' },
-                    { value: 20, name: 'Multimedia Processing' },
-                    { value: 20, name: 'Database' },
+                    { value: 30, name: 'Interaction Design' },
+                    { value: 10, name: 'Data Analysis' },
+                    { value: 30, name: 'Multimedia Processing' },
+                    { value: 10, name: 'Database' },
                     { value: 20, name: 'Graphics' },
                 ]
             }
@@ -368,7 +351,7 @@ function drawDonut_tech() {
                 data: [
                     { value: 40, name: 'C++, Qt'},
                     { value: 30, name: 'C#, Unity 3D' },
-                    { value: 20, name: 'OpenGL' },
+                    { value: 20, name: 'WebGL/OpenGL' },
                     { value: 10, name: 'JavaScript, CSS' },
                 ]
             }
